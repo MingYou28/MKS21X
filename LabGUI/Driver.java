8eb63ed0e8ad408e273272f9d9729f6b64cfc639
Ming You <mingyyou28@gmail.com>
2015-12-17 14:59:36 -0500

    Changed GUI interfaces/buttons

---
+++ LabGUI/Driver.java
@@ -0,0 +1,6 @@
+public class Driver {
+    public static void main(String[]args) {
+	TempConversionWindow GUI = new TempConversionWindow();
+	GUI.setVisible(true);
+    }
+}
---
+++ LabGUI/TempConversionWindow.java
@@ -0,0 +1,54 @@
+import javax.swing.*;
+import java.awt.*;//NEW STUFF!
+import java.awt.event.*;
+
+public class TempConversionWindow  extends JFrame implements ActionListener{
+    private Container pane;
+    private JLabel j;
+    private JTextField t;
+    
+    public TempConversionWindow() {
+	this.setTitle("Degree Conversion");
+	this.setSize(600,400);
+	this.setLocation(100,100);
+	this.setDefaultCloseOperation( EXIT_ON_CLOSE);
+	pane = this.getContentPane();
+	pane.setLayout(new BoxLayout(pane, BoxLayout.Y_AXIS));
+	
+	JButton b = new JButton("Convert from C to F");
+	b.addActionListener(this);
+	b.setActionCommand("ToF");
+	JButton b2 = new JButton("Convert from F to C");
+	b2.addActionListener(this);
+	b2.setActionCommand("ToC");
+	
+	
+	t = new JTextField(10);
+	
+	JCheckBox c = new JCheckBox("OverClock");
+	j = new JLabel("Temperature ");
+	pane.add(t);
+	pane.add(j);
+	pane.add(b);
+	pane.add(b2);
+    }
+    
+    public void actionPerformed(ActionEvent e){
+	String event = e.getActionCommand();
+	double degrees;
+	if(event.equals("ToF")){
+	    j.setText(""+CtoF(Double.parseDouble(j.getText())));}
+    
+	
+	if(event.equals("ToC")){
+	    j.setText(""+FtoC(Double.parseDouble(j.getText())));}
+  
+	
+    }
+    public double CtoF(double x){
+	return x * 9 / 5 + 32;
+    }
+    public double FtoC(double x){
+	return (x -32) * 5 / 9;
+    }
+}
